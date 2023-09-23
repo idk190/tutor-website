32be0cac230aa6e05d3576e2edf917877e7d1b8c
idk190 <mironmistry9@gmail.com>
2023-09-23 13:58:06 +0530

    Admin + Student User Name and Password UX Complete

--- static/js/login.js
+++ static/js/login.js
@@ -1,3 +1,9 @@
 function admin_signin() {
     admin_password.style.display = 'block';
+    student_username.style.display = 'none';
+}
+
+function student_signin() {
+    admin_password.style.display = 'none';
+    student_username.style.display = 'block';
 }
