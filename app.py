f94b95ef27dd1cf150615a9c4941ccb1b257885d
idk190 <mironmistry9@gmail.com>
2023-09-26 15:29:08 +0530

    Admin Page Database connected + Can add Tasks

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///video.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class Todo(db.Model):
    id = db.Column(db.Integer, primary_key=True)
-   title = db.Column(db.String(200), nullable=False)
+   content = db.Column(db.String(200), nullable=False)
    date_uploaded = db.Column(db.DateTime, default=datetime.utcnow)
 
    def __repr__(self):
@@ -46,6 +46,7 @@ VIDEOS = [
     'topic': 'Affine Transformations' 
   },
 ]
+
 @app.route("/", methods=['POST', 'GET'])
 def hello_world():
     return render_template('login.html', videos=VIDEOS)
@@ -56,7 +57,20 @@ def login():
 
 @app.route('/admin', methods=['POST', 'GET'])
 def admin():
-   return render_template('admin.html', videos=VIDEOS)
+   if request.method == 'POST':
+      task_content = request.form['content']
+      new_task = Todo(content=task_content)
+      
+
+      try:
+         db.session.add(new_task)
+         db.session.commit()
+         return redirect('/admin')
+      except: 
+         return 'There was an issue adding your task'
+   else:
+      tasks = Todo.query.order_by(Todo.date_uploaded).all()
+      return render_template('admin.html', videos=VIDEOS, tasks=tasks)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)
