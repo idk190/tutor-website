6a9b27474c30742eede17e37a3fc8ffab80956e9
idk190 <mironmistry9@gmail.com>
2023-10-01 06:10:45 +0530

    OHMYGODTHEFILTERWORKS

--- app.py
+++ app.py
@@ -14,58 +14,34 @@ class Video_List(db.Model):
    level = db.Column(db.String(200), nullable=False)
    difficulty = db.Column(db.String(200), nullable=False)
    topic = db.Column(db.String(200), nullable=False)
+   link = db.Column(db.String(200), nullable=False)
    date_uploaded = db.Column(db.DateTime, default=datetime.utcnow)
 
    def __repr__(self):
       return f'{self.id} {self.title}'
 
-VIDEOS = [
-  {
-    'id': 1,
-    'title': 'Chain Rule',
-    'level': '12',
-    'difficulty': 'Hard',
-    'topic': 'Calculus' 
-  },
-  {
-    'id': 2,
-    'title': 'Pythagorean Theorem',
-    'level': '10',
-    'difficulty': 'Easy',
-    'topic': 'Trigonometry' 
-  },
-  {
-    'id': 3,
-    'title': 'Binomial Distribution',
-    'level': '11',
-    'difficulty': 'Medium',
-    'topic': 'Statistics' 
-  },
-  {
-    'id': 4,
-    'title': 'Translations',
-    'level': '11',
-    'difficulty': 'Medium',
-    'topic': 'Affine Transformations' 
-  },
-]
+##VIDEOS = Video_List.query.all()##
 
 @app.route("/", methods=['POST', 'GET'])
 def hello_world():
-    return render_template('login.html', videos=VIDEOS)
+    listvid = Video_List.query.all()
+    return render_template('login.html', videos=listvid)
 
 @app.route('/home', methods=['POST', 'GET'])
 def login():
    listvid = Video_List.query.all()
-   return render_template('home.html', videos=VIDEOS)
+   return render_template('home.html', videos=listvid)
 
 @app.route('/admin', methods=['POST', 'GET'])
 def admin():
+   listvid = Video_List.query.all()
    if request.method == 'POST':
       task_title = request.form['title']
+      task_level = request.form['level']
       task_difficulty = request.form['difficulty']
       task_topic = request.form['topic']
-      new_task = Video_List(title= task_title, difficulty= task_difficulty, topic=task_topic)
+      task_link = request.form['link']
+      new_task = Video_List(title= task_title, level= task_level, difficulty= task_difficulty, topic=task_topic, link= task_link)
       
       
 
@@ -77,7 +53,7 @@ def admin():
          return 'There was an issue adding your task'
    else:
       tasks = Video_List.query.order_by(Video_List.date_uploaded).all()
-      return render_template('admin.html', videos=VIDEOS, tasks=tasks)
+      return render_template('admin.html', videos=listvid, tasks=tasks)
    
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -92,4 +68,4 @@ def delete(id):
    
 
 if __name__=="__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(debug=True)
